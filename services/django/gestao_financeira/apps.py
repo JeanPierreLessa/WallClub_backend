1e009e7755d6cc336849bb7f52b65c3a31d0c972
Jean Lessa <jeanpierre.lessa@gmail.com>
2025-11-20 18:35:00 -0300

    refactor: renomeia app de sistema_bancario para gestao_financeira

--- services/django/gestao_financeira/apps.py
+++ services/django/gestao_financeira/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SistemaBancarioConfig(AppConfig):
+class GestaoFinanceiraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sistema_bancario'
+    name = 'gestao_financeira'
