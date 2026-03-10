026de9861ffc75f083b554f774008b630483ab09
Jean Lessa <jeanpierre.lessa@gmail.com>
2026-03-09 21:07:45 -0300

    fix: alertmanager telegram config com envsubst + hook NEVER_EDIT_ON_SERVER

---
+++ monitoring/alertmanager-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Expande variáveis de ambiente no arquivo de configuração
+envsubst < /etc/alertmanager/alertmanager.yml > /tmp/alertmanager.yml.expanded
+mv /tmp/alertmanager.yml.expanded /etc/alertmanager/alertmanager.yml
+
+# Inicia o alertmanager
+exec /bin/alertmanager "$@"
