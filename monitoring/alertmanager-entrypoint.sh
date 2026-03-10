0e5e7d905fb2767e16468fac8dcb0fed8ec18f34
Jean Lessa <jeanpierre.lessa@gmail.com>
2026-03-09 21:51:52 -0300

    fix: update alertmanager service to build from custom Dockerfile and enhance entrypoint script for AWS Secrets Manager integration

--- monitoring/alertmanager-entrypoint.sh
+++ monitoring/alertmanager-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
+echo "Buscando credenciais do Telegram do AWS Secrets Manager..."
+
+SECRET_ID="wall/prod/db"
+
+# Buscar o secret completo
+SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --output text)
+
+# Extrair variáveis do Telegram
+export TELEGRAM_MONITOR_BOT_TOKEN=$(echo $SECRET_JSON | jq -r '.TELEGRAM_MONITOR_BOT_TOKEN // ""')
+export TELEGRAM_MONITOR_BOT_CHAT_ID=$(echo $SECRET_JSON | jq -r '.TELEGRAM_MONITOR_BOT_CHAT_ID // ""')
+
+echo "Credenciais carregadas: chat_id=$TELEGRAM_MONITOR_BOT_CHAT_ID"
+
 # Copia o arquivo template para um local write-able e expande variáveis
 cp /etc/alertmanager/alertmanager.yml /tmp/alertmanager.yml.template
 
