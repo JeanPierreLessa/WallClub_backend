33569f70359040be21ba359fbac92874e32498d2
Jean Lessa <jeanpierre.lessa@gmail.com>
2026-03-09 21:31:34 -0300

    fix: update alertmanager entrypoint to use sed for environment variable expansion

--- monitoring/alertmanager-entrypoint.sh
+++ monitoring/alertmanager-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
-# Expande variáveis de ambiente no arquivo de configuração
-envsubst < /etc/alertmanager/alertmanager.yml > /tmp/alertmanager.yml.expanded
-mv /tmp/alertmanager.yml.expanded /etc/alertmanager/alertmanager.yml
+# Expande variáveis de ambiente no arquivo de configuração usando sed
+cp /etc/alertmanager/alertmanager.yml /tmp/alertmanager.yml.template
+
+sed -e "s|\${TELEGRAM_MONITOR_BOT_TOKEN}|${TELEGRAM_MONITOR_BOT_TOKEN}|g" \
+    -e "s|\${TELEGRAM_MONITOR_BOT_CHAT_ID}|${TELEGRAM_MONITOR_BOT_CHAT_ID}|g" \
+    /tmp/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
 
 # Inicia o alertmanager
 exec /bin/alertmanager "$@"
